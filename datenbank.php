96b6856af3c200fba2341bc22a9c6f6f021c8dad
Auke Swen <aukeswen2001@yahoo.com>
2018-03-14 12:08:58 +0100

    mit datenbank

--- datenbank.php
+++ datenbank.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-    $connString = "mysql:host=127.0.0.1;port=3306;dbname=haushaltsbuch";
+    $connString = "mysql:host=127.0.0.1;port=3306;dbname=haushaltsbuch;charset=utf8";
 
     $db = new PDO($connString, "root", "");
 
@@ -16,25 +16,4 @@ catch(PDOException $error)
 	print "Nachricht: ".$error->getMessage();
 }
 
-/* $befehl = "select sum(betrag) as summe from ausgaben join rechnung on rechnung.id = ausgaben.rechnungsnr where week(datum,1) = week(current_date,1) and einmalig = 1;";
-$result = $db->query($befehl);
-$daten = $result->fetch(PDO::FETCH_ASSOC);
-$gesamt_woche = $daten["summe"];
-
-$befehl = "select sum(betrag) as summe from ausgaben join rechnung on rechnung.id = ausgaben.rechnungsnr join produktgruppe on produktgruppe.id = ausgaben.prod_gr where week(datum,1) = week(current_date,1) and einmalig = 1 and produktgruppe.essen = 1;";
-$result = $db->query($befehl);
-$daten = $result->fetch(PDO::FETCH_ASSOC);
-$gesamt_woche_essen = $daten["summe"];
-
-$befehl = "select sum(betrag) as summe from ausgaben join rechnung on rechnung.id = ausgaben.rechnungsnr join produktgruppe on produktgruppe.id = ausgaben.prod_gr where week(datum,1) = week(current_date,1) and einmalig = 1 and produktgruppe.essen = 0;";
-$result = $db->query($befehl);
-$daten = $result->fetch(PDO::FETCH_ASSOC);
-$gesamt_woche_sontiges = $daten["summe"];
-
-$befehl = "select produktgruppe.bezeichnung, sum(betrag) as summe from ausgaben join rechnung on rechnung.id = ausgaben.rechnungsnr join produktgruppe on produktgruppe.ID = ausgaben.prod_gr where week(rechnung.datum,1) = week(current_date,1) and rechnung.einmalig = 1 group by produktgruppe.bezeichnung order by summe desc;";
-$result = $db->query($befehl);
-$kategorien = $result->fetchAll();
-
-
-$db = null; */
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require("wochenuebersicht.php");
+header('Content-type: text/html; charset=utf-8');
 ?>
 <!DOCTYPE html>
 <html>
